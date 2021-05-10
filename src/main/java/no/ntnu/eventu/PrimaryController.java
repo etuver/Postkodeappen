2a57f81458bacb47cabd07208063c5ee51bb7d57
Even Tuverud <eventu@stud.ntnu.no>
2021-05-10 20:32:08 +0200

    Moving actionevents to own methods

--- src/main/java/no/ntnu/eventu/PrimaryController.java
+++ src/main/java/no/ntnu/eventu/PrimaryController.java
@@ -3,47 +3,66 @@ package no.ntnu.eventu;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PrimaryController {
 
-
-    public TextField searchTxt;
-    public Button showAllBtn;
-    public Button searchBtn;
-    public Label errorLabel;
+    //Tableview
     @FXML
     private TableView postalAdressTable;
+
+    //Textfield for user input with search text
+    @FXML
+    public TextField searchTxt;
+
+    //Buttons
+    @FXML
+    public Button showAllBtn;
+    @FXML
+    public Button searchBtn;
+    @FXML
+    public Label errorLabel;
+
+    //Menu items
+    @FXML
+    public MenuItem closeMenuBtn;
+    @FXML
+    public MenuItem helpMenuBtn;
+    @FXML
+    public MenuItem aboutMenuBtn;
+
+
     /**
-     * Get postal adress register
+     * Get postal address register
      */
     PostalAdressRegister postalAdressRegister = PostalAdressRegister.getInstance();
 
+    /**
+     * Get FileManager
+     */
     FileManager fileManager = new FileManager();
 
 
-
-    private void addTestData(){
-        postalAdressRegister.registerPostalAdress("0200","Oslo", "4","Oslo","B");
-        postalAdressRegister.registerPostalAdress("2020","Skedsmokorset", "5","Skedsmo","G");
-        postalAdressRegister.registerPostalAdress("7020","Trondheim", "6","Trøndelag","S");
-        postalAdressRegister.registerPostalAdress("7500","Steinkjer", "7","Trøndelag","G");
+    /**
+     * Some test data
+     */
+    private void addTestData() {
+        postalAdressRegister.registerPostalAdress("0200", "Oslo", "4", "Oslo", "B");
+        postalAdressRegister.registerPostalAdress("2020", "Skedsmokorset", "5", "Skedsmo", "G");
+        postalAdressRegister.registerPostalAdress("7020", "Trondheim", "6", "Trøndelag", "S");
+        postalAdressRegister.registerPostalAdress("7500", "Steinkjer", "7", "Trøndelag", "G");
     }
 
 
-
-
-
     @FXML
-    private void initialize() throws FileNotFoundException {
+    private void initialize() {
         //addTestData();
-        try {
-            fileManager.readFromFile(postalAdressRegister, "src/main/resources/no/ntnu/eventu/Postnummerregister.csv");
-        }catch (FileNotFoundException f){
-            errorLabel.setText(f.getMessage());
-        }
+        importData();
+        handleShowAll();
 
         TableColumn<PostalAdress, String> postalCodeColumn = new TableColumn<>("Postnummer");
         postalCodeColumn.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
@@ -61,29 +80,77 @@ public class PrimaryController {
         postalCodeCategoryColumn.setCellValueFactory(new PropertyValueFactory<>("postalAdressCategory"));
         postalAdressTable.getColumns().add(postalCodeCategoryColumn);
 
+        helpMenuBtn.setOnAction(event -> {
+            handleHelp();
+        });
+
+        aboutMenuBtn.setOnAction(event -> {
+            handleAbout();
+        });
+
+        closeMenuBtn.setOnAction(event -> {
+            handleClose();
+        });
 
         showAllBtn.setOnAction(event -> {
-            postalAdressTable.getItems().clear();
-            postalAdressTable.getItems().addAll(postalAdressRegister.getPostalAdressRegister());
-                });
+            handleShowAll();
+        });
 
         searchBtn.setOnAction(event -> {
-            postalAdressTable.getItems().clear();
-
-            if (searchTxt.getText().matches("^\\d|\\d{2}|\\d{3}|\\d{4}$")){
-                postalAdressTable.getItems().addAll(postalAdressRegister.searchByPostalCode(searchTxt.getText()));
-            }else {
-                postalAdressTable.getItems().addAll(postalAdressRegister.searchByAdress(searchTxt.getText()));
-            }
-
+            handleSearch();
         });
+    }
+
+    private void handleSearch() {
+        if (searchTxt.getText().length() <2 || searchTxt.getText().isBlank()){
+            errorLabel.setText("Vennligst fyll inn minst to tall eller bokstaver for å søke");
+        }else {
+        clearTable();
+        if (searchTxt.getText().matches("^\\d|\\d{2}|\\d{3}|\\d{4}$")) {
+            postalAdressTable.getItems().addAll(postalAdressRegister.searchByPostalCode(searchTxt.getText()));
+        } else {
+            postalAdressTable.getItems().addAll(postalAdressRegister.searchByAdress(searchTxt.getText()));
+        }}
+    }
+
+    private void handleShowAll() {
+        clearTable();
+        postalAdressTable.getItems().addAll(postalAdressRegister.getPostalAdressRegister());
+    }
+
+    private void handleClose() {
+        Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION, "Er du sikker på at du vil avslutte?", ButtonType.YES, ButtonType.NO);
+        exitAlert.setTitle("Confirm exit");
+        exitAlert.setHeaderText("Avslutte?");
+        exitAlert.setAlertType(Alert.AlertType.WARNING);
+        exitAlert.showAndWait();
+        if (exitAlert.getResult() == ButtonType.YES){
+            Platform.exit();
+        }
+    }
+
+    private void handleHelp() {
 
     }
 
+    private void handleAbout() {
 
+    }
 
+    private void importData() {
+        try {
+            fileManager.readFromFile(postalAdressRegister, "src/main/resources/no/ntnu/eventu/Postnummerregister.txt");
+        } catch (FileNotFoundException f) {
+            errorLabel.setText(f.getMessage());
+        }
+    }
 
-
+    /**
+     * Method to clear the tableview
+     */
+    private void clearTable() {
+        postalAdressTable.getItems().clear();
+    }
 
 
 }
