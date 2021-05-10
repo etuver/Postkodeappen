63e3c1c8403d5b416c595138deb1b15f46de972a
Even Tuverud <eventu@stud.ntnu.no>
2021-05-10 16:48:51 +0200

    basic implementation of the class

--- src/main/java/no/ntnu/eventu/PostalAdressRegister.java
+++ src/main/java/no/ntnu/eventu/PostalAdressRegister.java
@@ -1,4 +1,49 @@
 package no.ntnu.eventu;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PostalAdressRegister {
+
+    private ArrayList<PostalAdress> postalAdressRegister;
+
+    public PostalAdressRegister(){
+        this.postalAdressRegister = new ArrayList<>();
+    }
+
+    public void registerPostalAdress(String postalCode, String postalAdress, String municipalityCode, String municipalityName, String postalAdressCategory){
+        postalAdressRegister.add(new PostalAdress(postalCode,postalAdress,municipalityCode,municipalityName,postalAdressCategory));
+    }
+
+
+    public ArrayList<PostalAdress> getPostalAdressRegister() {
+        return postalAdressRegister;
+    }
+
+    public ArrayList<PostalAdress> searchByPostalCode(String postalCode){
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < postalAdressRegister.size(); i++){
+            if (postalAdressRegister.get(i).getPostalCode().equals(postalCode)){
+                result.add(postalAdressRegister.get(i));
+            }
+        }
+        return result;
+    }
+
+
+    public List<PostalAdress> searchByAdress(String adress){
+        List<PostalAdress> matches = postalAdressRegister.stream()
+                .filter(p -> p.getPostalAdress().contains(adress) || p.getMunicipalityName().contains(adress))
+                .collect(Collectors.toList());
+        return matches;
+    }
+
+    private static final PostalAdressRegister instance = new PostalAdressRegister();
+
+    public static PostalAdressRegister getInstance(){
+        return instance;
+
+    }
+
 }
--- src/main/java/no/ntnu/eventu/PrimaryController.java
+++ src/main/java/no/ntnu/eventu/PrimaryController.java
@@ -5,4 +5,22 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    /**
+     * Get postal adress register
+     */
+    PostalAdressRegister postalAdressRegister = PostalAdressRegister.getInstance();
+
+    @FXML
+    private void initialize(){
+        System.out.println("fdasfdsdfs");
+
+
+
+
+    }
+
+
+
+
+
 }
