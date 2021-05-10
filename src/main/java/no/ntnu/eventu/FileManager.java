c099e06cadd06b23c10d360e18155b3ff3917d64
Even Tuverud <eventu@stud.ntnu.no>
2021-05-10 18:37:13 +0200

    Make method to read from file

--- src/main/java/no/ntnu/eventu/FileManager.java
+++ src/main/java/no/ntnu/eventu/FileManager.java
@@ -1,4 +1,22 @@
 package no.ntnu.eventu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileManager {
+
+    public void readFromFile(PostalAdressRegister postalAdressRegister, String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            String[] lineInfo = line.split("\t");
+            postalAdressRegister.registerPostalAdress(lineInfo[0], lineInfo[1], lineInfo[2], lineInfo[3], lineInfo[4]);
+        }
+        scanner.close();
+    }
+
+
+
+
 }
